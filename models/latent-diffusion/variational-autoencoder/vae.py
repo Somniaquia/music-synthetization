040ec1c7289078c88d81f5685406b30796efc1d6
Somniaquia <Somniaquia@gmail.com>
2023-10-26 21:38:53 +0900

    Colab change

--- models/latent-diffusion/variational-autoencoder/vae.py
+++ models/latent-diffusion/variational-autoencoder/vae.py
@@ -455,7 +455,7 @@ if __name__ == "__main__":
 
     model = VAE()
 
-    train_set = MusicDataset(root_dir='data\\16kHz_8bit')
+    train_set = MusicDataset(root_dir=input("Data direrctory: "))
 
     train_loader = DataLoader(train_set, batch_size=1, shuffle=True, num_workers=16, collate_fn=collate_fn)
     val_loader = DataLoader(train_set, batch_size=1, num_workers=16, collate_fn=collate_fn)
