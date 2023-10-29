a82081e36c07c26b3ae7117adfad880557710334
Somniaquia <Somniaquia@gmail.com>
2023-10-29 20:51:29 +0900

    Added DDPM and U-Net

--- models/latent-diffusion/variational-autoencoder/vae.py
+++ models/latent-diffusion/variational-autoencoder/vae.py
@@ -9,6 +9,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
 
+from ..blocks import *
+
 torch.set_float32_matmul_precision('medium')
 
 def get_timestep_embedding(timesteps, embedding_dim):
@@ -31,104 +33,6 @@ def get_timestep_embedding(timesteps, embedding_dim):
 
 def Normalize(in_channels, num_groups=32):
     return nn.GroupNorm(num_groups=num_groups, num_channels=in_channels, eps=1e-6, affine=True)
-
-class Upsample(nn.Module):
-    def __init__(self, in_channels, with_conv):
-        super().__init__()
-        self.with_conv = with_conv
-        if self.with_conv:
-            self.conv = nn.Conv1d(in_channels, in_channels, kernel_size=3, stride=1, padding=1)
-
-    def forward(self, x):
-        x = F.interpolate(x, scale_factor=2.0, mode='linear')  # Changed to 'linear' for smoother results
-        if self.with_conv:
-            x = self.conv(x)
-        return x
-    
-class Downsample(nn.Module):
-    def __init__(self, in_channels, with_conv):
-        super().__init__()
-        self.with_conv = with_conv
-        if self.with_conv:
-            self.conv = nn.Conv1d(in_channels, in_channels, kernel_size=4, stride=2, padding=1)
-
-    def forward(self, x):
-        if self.with_conv:
-            x = self.conv(x)
-        else:
-            x = F.avg_pool1d(x, kernel_size=2, stride=2)
-        return x
-
-
-class ResnetBlock(nn.Module):
-    def __init__(self, *, in_channels, out_channels=None, conv_shortcut=False, dropout, t_emb_channels=512):
-        super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = in_channels if out_channels is None else out_channels
-        out_channels = self.out_channels
-        self.use_conv_shortcut = conv_shortcut
-
-        self.norm1 = nn.LayerNorm(in_channels)
-        self.conv1 = nn.Conv1d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
-
-        if t_emb_channels > 0:
-            self.t_emb_proj = nn.Linear(t_emb_channels, out_channels)
-        
-        self.norm2 = nn.LayerNorm(out_channels)
-        self.dropout = nn.Dropout(dropout)
-        self.conv2 = nn.Conv1d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
-        
-        if self.in_channels != self.out_channels:
-            if self.use_conv_shortcut:
-                self.conv_shortcut = nn.Conv1d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
-            else:
-                # The NiN (Network-in-Network) shortcut - https://arxiv.org/abs/1312.4400
-                self.nin_shortcut = nn.Conv1d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
-
-    def forward(self, x, t_emb):
-        h = x
-
-        h = self.norm1(h.transpose(1, 2)).transpose(1, 2)
-        h = nn.SiLU()(h)
-        h = self.conv1(h)
-
-        # Project t_emb to the same dimensionality as the output of the first convolutional layer, add to the result
-        if t_emb is not None:
-            # The original network processed 4D image tensors (b, h, w, c) but in case of 2D audio tensors (b, s) dimensionality augmentation is unnecessary
-            # h = h + self.t_emb_proj(nn.SiLU(t_emb))[:,:,None,None]
-            h = h + self.t_emb_proj(nn.SiLU()(t_emb))[:, :, None]
-
-        h = self.norm2(h.transpose(1, 2)).transpose(1, 2)
-        h = nn.SiLU()(h)
-        h = self.dropout(h)
-        h = self.conv2(h)
-
-        if self.in_channels != self.out_channels:
-            if self.use_conv_shortcut:
-                x = self.conv_shortcut(x)
-            else:
-                x = self.nin_shortcut(x)
-
-        # Implement the defining skip connection of a ResnetBlock
-        return x+h
-
-class LinearAttention(nn.Module):
-    def __init__(self, dim, heads=4, dim_head=32):
-        super().__init__()
-        self.heads = heads
-        hidden_dim = dim_head * heads
-        self.to_qkv = nn.Conv1d(dim, hidden_dim * 3, 1, bias = False)
-        self.to_out = nn.Conv1d(hidden_dim, dim, 1)
-
-    def forward(self, x):
-        b, c, l = x.shape
-        qkv = self.to_qkv(x)
-        q, k, v = rearrange(qkv, 'b (qkv heads c) l -> qkv b heads c l', heads=self.heads, qkv=3)
-        k = k.softmax(dim=-1)
-        context = torch.einsum('bhdn,bhen->bhde', k, v)
-        out = torch.einsum('bhde,bhdn->bhen', context, q)
-        out = rearrange(out, 'b heads c l -> b (heads c) l', heads=self.heads, l=l)
-        return self.to_out(out)
 
 # Gaussian Distribution with a diagonal covariance matrix
 class DiagonalGaussianDistribution(object):
@@ -344,21 +248,16 @@ class Decoder(nn.Module):
         h = self.mid.block_2(h, t_emb)
 
         for i_level in reversed(range(self.num_resolutions)):
-            for i_block in range(self.num_res_blocks + 1):
+            for i_block in range(self.num_res_blocks):
                 h = self.up[i_level].block[i_block](h, t_emb)
                 if len(self.up[i_level].attn) > 0:
                     h = self.up[i_level].attn[i_block](h)
             if i_level != 0:
                 h = self.up[i_level].upsample(h)
-
-        if self.give_pre_end:
-            return h
         
         h = self.norm_out(h.transpose(1, 2)).transpose(1, 2)
         h = nn.SiLU()(h)
         h = self.conv_out(h)
-        if self.tanh_out:
-            h = torch.tanh(h)
         return h
 
 
@@ -369,7 +268,7 @@ class VAE(pl.LightningModule):
         self.decoder = Decoder(ch=64, ch_mult=(8, 4, 2, 1), resolution=4800000, z_channels=512)
         self.loss = CombinedAudioLoss(alpha=0.5)
 
-        self.quant_conv = nn.Conv1d(512, embed_dim, 1)  # Matched to z_channels
+        self.quant_conv = nn.Conv1d(512, embed_dim*2, 1)  # Matched to z_channels
         self.post_quant_conv = nn.Conv1d(embed_dim, 512, 1)  # Matched to z_channels
         self.embed_dim = embed_dim
         self.learning_rate = learning_rate
@@ -447,17 +346,14 @@ if __name__ == "__main__":
 
     model = VAE()
     print(model)
-    x = torch.randn(1, 1, 4800000) 
-    y = model(x)
-    # torchviz.make_dot(y, params=dict(list(model.named_parameters()) + [('x', x)]))
 
-    # train_set = MusicDataset(root_dir=input("Data direrctory: "))
+    train_set = MusicDataset(root_dir=input("Data direrctory: "))
 
-    # train_loader = DataLoader(train_set, batch_size=1, shuffle=True, num_workers=1, collate_fn=collate_fn)
-    # val_loader = DataLoader(train_set, batch_size=1, num_workers=1, collate_fn=collate_fn)
-    # for batch in train_loader:
-    #     print("Batch shape:", batch.shape)
-    #     break
+    train_loader = DataLoader(train_set, batch_size=1, shuffle=True, num_workers=1, collate_fn=collate_fn)
+    val_loader = DataLoader(train_set, batch_size=1, num_workers=1, collate_fn=collate_fn)
+    for batch in train_loader:
+        print("Batch shape:", batch.shape)
+        break
 
-    # trainer = pl.Trainer(max_epochs=100, precision='16-mixed')
-    # trainer.fit(model, train_loader, val_loader)
+    trainer = pl.Trainer(max_epochs=100, precision='16-mixed')
+    trainer.fit(model, train_loader, val_loader)
